4ad912813091113bbb7e096a691840e86af621df
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-02-17 18:46:03 -0600

    Feat: update config server set up per MongoDB3.4

--- init_sharded_env__m101p_windows_sz.sh
+++ init_sharded_env__m101p_windows_sz.sh
@@ -8,8 +8,8 @@
 echo "killing mongod and mongos"
 # killall mongod
 # killall mongos
-# taskkill /IM mongod.exe  TODO: does not work
-# taskkill /IM mongos.exe  TODO: does not work
+taskkill /im mongod.exe /f
+taskkill /im mongos.exe /f
 # TODO: for now, manually do below:
 # tasklist | grep mongod
 # tasklist | grep mongos
@@ -17,8 +17,11 @@ echo "killing mongod and mongos"
 
 echo "removing data files"
 rm -rf \data\config
-rm -rf \data\shard*
 
+rm -rf \data\shard*
+rm "cfg-a.log" "cfg-b.log" "cfg-c.log" "s0-r0.log" "s0-r1.log" "s0-r2.log" "s1-r0.log" "s1-r1.log" "s1-r2.log"
+rm "s2-r0.log" "s2-r1.log" "s2-r2.log"
+rm "mongos-1.log"
 
 # start a replica set and tell it that it will be shard0
 echo "starting servers for shard 0"
@@ -78,16 +81,28 @@ EOF
 # now start 3 config servers
 echo "Starting config servers"
 mkdir \data\config\config-a \data\config\config-b \data\config\config-c
-start mongod --logpath "cfg-a.log" --dbpath \data\config\config-a --port 57040 --configsvr --smallfiles
-start mongod --logpath "cfg-b.log" --dbpath \data\config\config-b --port 57041 --configsvr --smallfiles
-start mongod --logpath "cfg-c.log" --dbpath \data\config\config-c --port 57042 --configsvr --smallfiles
+start mongod --replSet csReplSet --logpath "cfg-a.log" --dbpath \data\config\config-a --port 57040 --configsvr --smallfiles
+start mongod --replSet csReplSet --logpath "cfg-b.log" --dbpath \data\config\config-b --port 57041 --configsvr --smallfiles
+start mongod --replSet csReplSet --logpath "cfg-c.log" --dbpath \data\config\config-c --port 57042 --configsvr --smallfiles
 
+sleep 5
+
+echo "Configuring csReplSet replica set"
+mongo --port 57040 << 'EOF'
+rs.initiate( {
+   _id: "csReplSet",
+   configsvr: true,
+   version: 1,
+   members: [ { _id: 0, host: "localhost:57040" },
+   			  { _id: 1, host: "localhost:57041" },
+   			  { _id: 2, host: "localhost:57042" } ]});
+EOF
 
 # now start the mongos on a standard port
-start mongos --logpath "mongos-1.log" --configdb localhost:57040,localhost:57041,localhost:57042
-echo "Waiting 60 seconds for the replica sets to fully come online"
-sleep 60
-# TODO: have not figured out how to do sharing in scripts, only work manually on Windows for me
+mongos --logpath "mongos-1.log" --configdb csReplSet/localhost:57040,localhost:57041,localhost:57042
+# echo "Waiting 60 seconds for the replica sets to fully come online"
+# sleep 60
+# sharding
 echo "Connnecting to mongos and enabling sharding"
 
 # add shards and enable sharding on the test db
@@ -95,8 +110,8 @@ mongo <<'EOF'
 db.adminCommand( { addshard : "s0/"+"localhost:37017" } );
 db.adminCommand( { addshard : "s1/"+"localhost:47017" } );
 db.adminCommand( { addshard : "s2/"+"localhost:57017" } );
-db.adminCommand({enableSharding: "school"})
-db.adminCommand({shardCollection: "school.students", key: {student_id:1}});
+db.adminCommand({enableSharding: "schooldb"})
+db.adminCommand({shardCollection: "schooldb.students", key: {student_id:1}});
 EOF
 
 
